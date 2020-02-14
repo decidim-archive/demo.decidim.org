86895645338270dc84d4b20a0be2cfadc37f9bca
Andrés Pereira de Lucena <apereira@alabs.org>
2020-02-14 11:42:39 +0100

    Config RAILS_LOG_TO_STDOUT to every environment

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,12 @@ module DemoDecidimOrg
 
     config.active_job.queue_adapter = :delayed_job
 
+    if ENV["RAILS_LOG_TO_STDOUT"].present?
+      logger           = ActiveSupport::Logger.new(STDOUT)
+      logger.formatter = config.log_formatter
+      config.logger    = ActiveSupport::TaggedLogging.new(logger)
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
